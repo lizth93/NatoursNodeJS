7546ab45d7dbfaacf1776dae30b78f14482a1e32
lizth93 <lizth93@hotmail.com>
2023-07-18 10:28:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.use(express.json()); //middleware+
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app runing on port ${port}`);
+});
